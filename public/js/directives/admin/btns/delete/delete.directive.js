ab5d21ef599ae2a60ec6d32f16abd5e2e9c7927e
David Corkett <dcorkett@gmail.com>
2017-06-05 21:35:06 +0100

    added delete btns and directives; added orderBY for allRecipes

--- public/js/directives/admin/btns/delete/delete.directive.js
+++ public/js/directives/admin/btns/delete/delete.directive.js
@@ -1,27 +1,31 @@
-// angular
-//   .module('myApp')
-//   .directive('delete', deleteBtn);
-//
-// function deleteBtn () {
-//   return {
-//     restrict: 'E',
-//     scope: {
-//       del: '='
-//     },
-//
-//     templateUrl: 'js/directives/admin//btns/delete/delete.directive.html',
-//
-//     link: function (scope, element, attrs) {
-//       scope.allowDelete = false;
-//
-//       scope.toggleDelete = function () {
-//         scope.allowDelete = !scope.allowDelete;
-//       };
-//
-//       scope.delt = function (deleteRecipe) {
-//         // deleteRecipe();
-//       };
-//
-//     }
-//   };
-// }
+angular
+  .module('myApp')
+  .directive('deletebtn', deleteBtn);
+
+function deleteBtn () {
+  return {
+    restrict: 'E',
+    scope: {
+      cntr: '=',
+      recipeid: '=',
+      del: '='
+    },
+
+    templateUrl: 'js/directives/admin//btns/delete/delete.directive.html',
+
+    link: function (scope, element, attrs) {
+      scope.allowDelete = false;
+
+      scope.toggleDelete = function () {
+        scope.allowDelete = !scope.allowDelete;
+      };
+
+      scope.removeEmptyObj = function () {
+        console.log('cntr', scope.cntr);
+        console.log('recipeid', scope.recipeid);
+        scope.cntr(scope.recipeid);
+      };
+
+    }
+  };
+}
